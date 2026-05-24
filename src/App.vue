d42993c4c2fc7b745a4c7ed36e3a1621900ed4e8
Travis <146148+rockacola@users.noreply.github.com>
2026-05-24 12:36:06 +1000

    chore: add gradient background

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="flex min-h-screen flex-col bg-gradient-to-b from-sky-100 from-70% to-sky-200">
     <RouterView class="flex-1" />
   </div>
 </template>
